204dc70bd6f8b8e4a0710588e011b8129312fcb2
max <max.baur@barmer.de>
2026-02-17 00:21:13 +0100

    destroy wird schon funktionieren

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -60,3 +60,27 @@ def deploy(app: AwsApp, config_dir: Path = Path("config")) -> IacMapping:
 
     desired_iac_mapping.to_yaml(config_file)
     return iac_mapping
+
+
+def destroy(app: AwsApp, config_dir: Path = Path("config")) -> None:
+    config_dir.mkdir(parents=True, exist_ok=True)
+    config_file = config_dir / f"app_{app.name}.yaml"
+
+    try:
+        iac_mapping: IacMapping = IacMapping.from_yaml(config_file)
+    except ValidationError as e:
+        raise RuntimeError(f"Invalid config {config_file}:\n{e}")
+
+    # Lösche alle Ressourcen aus der Config
+    for resource_id in iac_mapping.resources:
+        resource_mapping = iac_mapping.resources[resource_id]
+        resource_type = resource_mapping.type
+        tech_id = resource_mapping.tech_id
+
+        if resource_type == "s3":
+            resource = S3.get(tech_id, app.env)
+            resource.delete(tech_id)
+
+    # Leere die Config
+    empty_mapping = IacMapping()
+    empty_mapping.to_yaml(config_file)
