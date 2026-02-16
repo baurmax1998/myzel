985ee0d6acd5c8d5fe4a5dcb122dcde228b1539f
max <max.baur@barmer.de>
2026-02-16 23:14:32 +0100

    alle daten für den diff vorbereitet

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -4,6 +4,7 @@ import yaml
 from pydantic import BaseModel, Field, ValidationError
 
 from src.model import AwsApp
+from src.resources import Resources
 from src.resources.s3 import S3
 
 
@@ -39,11 +40,16 @@ def deploy(app: AwsApp, config_dir: Path = Path("config")) -> IacMapping:
     except ValidationError as e:
         raise RuntimeError(f"Invalid config {config_file}:\n{e}")
 
+    deployed_constructs: dict[str, Resources] = {}
     for resource_id in iac_mapping.resources:
         resource_mapping = iac_mapping.resources[resource_id]
         resource_type = resource_mapping.type
         if resource_type == "s3":
             resource = S3.get(resource_mapping.tech_id, app.env)
+            deployed_constructs[resource_id] = resource
+
+    desired_constructs: dict[str, Resources] = app.constructs
+
 
 
     iac_mapping.to_yaml(config_file)
