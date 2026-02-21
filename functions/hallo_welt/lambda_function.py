81d63ecba2e4a07606c2959b978c98e8dbc27004
max <max.baur@barmer.de>
2026-02-21 23:37:51 +0100

    hello world lambda

---
+++ functions/hallo_welt/lambda_function.py
@@ -0,0 +1,20 @@
+
+def lambda_handler(event, context):
+    """
+    Lambda Handler für Hallo Welt Funktion.
+
+    Args:
+        event: Lambda Event mit optionalem 'name' Parameter
+        context: Lambda Context
+
+    Returns:
+        dict: Response mit Statuscode und Body
+    """
+    name = event.get('name', 'Welt')
+
+    message = f"Hallo {name}"
+
+    return {
+        'statusCode': 200,
+        'body': message
+    }
