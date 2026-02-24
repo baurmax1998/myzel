71c18fbec65ab3bd472c24b62a8126b53bd04669
max <max.baur@barmer.de>
2026-02-24 13:47:38 +0100

    test vorbereitet

--- test/resources/generic_resources_test.py
+++ test/resources/generic_resources_test.py
@@ -3,12 +3,16 @@
 def main():
     # create Env
     # init Resource
-    # put Resource erstell Resource in der Cloud
+    # create Resource erstell Resource in der Cloud
     # get Resource state von der Resource in der cloud
-    # put Resource wird in der coud geupdated
+    # create Resource nichts passiert weil die Resource schon existiert
+    # get Resource state passt zu der änderung
+    # update Resource mit der id von create => resource wird geupdated
+    # get Resource state passt zu der änderung
+    # update Resource einer falschen id von create => resource wird erstellt
     # get Resource state passt zu der änderung
     # delete Resource löscht die Resource in der cloud
-    # delete Resource ist ja schon gelöscht aber es kommt kein fehler
+    # delete Resource löscht die neue "Update" Resource in der cloud
+    # delete Resource ist ja schon gelöscht, aber es kommt kein fehler
     # get Resource leerer state, kein fehler
-
     pass
